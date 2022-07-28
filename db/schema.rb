baaed8d6c6f777dff24ccdcd372577bb6d39f83c
Aleem <muhammad.aleem@visnext.com>
2022-07-28 17:14:55 +0500

    add one to many assiciation between recipe and chef

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_28_104249) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_28_113057) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_104249) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chef_id"
   end
 
 end
