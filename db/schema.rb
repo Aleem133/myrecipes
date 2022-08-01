0f9c26aa88f77afa87996532b4a28ea581d6a2b9
Aleem <muhammad.aleem@visnext.com>
2022-08-01 17:44:30 +0500

    Complete authentication with Admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_150738) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_01_115940) do
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "recipes", force: :cascade do |t|
